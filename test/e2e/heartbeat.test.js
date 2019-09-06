3470bebe732968b2b876ece3ef01502c9ecc6414
Simon Renoult <simon.renoult@gmail.com>
2019-09-06 10:45:02 +0200

    feat: better e2e tests and upgrade deps

--- test/e2e/heartbeat.test.js
+++ test/e2e/heartbeat.test.js
@@ -1,12 +1,8 @@
-const { expect } = require("chai");
-const request = require("got");
+const { client, describeWithApi, expect } = require("./utils");
 
-describe("GET /hearbeat", () => {
-  it("returns {isAlive: true}", async function() {
-    // when
-    const { body } = await request.get("/heartbeat", this.requestOptions);
-
-    // then
-    expect(body).to.deep.equal({ isAlive: true });
+describeWithApi("GET /heartbeat", () => {
+  it("returns 200", async () => {
+    const response = await client.get("/heartbeat");
+    expect(response.statusCode).to.equal(200);
   });
 });
---
+++ test/e2e/utils.js
@@ -0,0 +1,27 @@
+const { describe } = require("mocha");
+const { createServer } = require("http");
+const { expect } = require("chai");
+const { extend } = require("got");
+
+const server = require("../../src/application");
+
+const configuration = server.get("configuration");
+const port = configuration.get("port");
+const client = extend({ baseUrl: `http://localhost:${port}` });
+
+function describeWithApi(describeTitle, callback) {
+  let api;
+
+  before(done => {
+    api = createServer(server);
+    api.listen(port, done);
+  });
+
+  after(done => {
+    api.close(done);
+  });
+
+  describe(describeTitle, callback);
+}
+
+module.exports = { client, describeWithApi, expect };
